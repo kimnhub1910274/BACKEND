d4e78342fdba0277af5d1dd912717630dc814226
nhub1910274 <nhub1910274@student.ctu.edu.vn>
2023-02-15 16:07:51 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -7,20 +7,19 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("api/contacts", contactsRouter);
+app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
-    res.json({message: "Welcom to contact book application."});
+    res.json({message: "Welcome to contact book application."});
 });
-// app.use("/api/contacts", contactsRouter);
 module.exports = app;
 
-// app.use((req, res, next) => {
-//     return next(new ApiError(404, "Resource not found"));
-// });
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
 
-// app.use((err, req, res, next) => {
-//     return res.status(error.statusCode || 500).json({
-//         message: error.message || "Internal Saver Error",
-//     });
-// });
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Saver Error",
+    });
+});
